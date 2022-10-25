b5b88bbe81b79be0f4c3afa46bfef91b82f7c80f
Raihan <rhnchdri@gmail.com>
2022-10-25 15:28:53 +0600

    navbar part

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -2,27 +2,19 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDrsopdown';
 import logo from '../logo/logo.jpg'
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <img
-                    src={logo}
-                    width="30"
-                    height="30"
-                    className="align-top rounded-circle"
-                    alt=""
-                />
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <img src={logo} width="30" height="30" className="align-top rounded-circle me-2" alt="" />
+                <Navbar.Brand href="#home">CS for knowing & doing</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-
+                        <Nav.Link href="#features">Home</Nav.Link>
+                        <Nav.Link href="#pricing">Courses</Nav.Link>
                     </Nav>
                     <Nav>
                         <Nav.Link href="#deets">More deets</Nav.Link>
